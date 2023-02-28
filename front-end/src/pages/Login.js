04a543c94dfe207e0b6b1cd3dec90d7103c809e1
Cassio <superkoalabr@gmail.com>
2023-02-28 18:18:57 -0300

    requisito 3 finalizado

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 
+const SIX = 6;
+
 export default function Login() {
+  const [email, setChangeEmail] = useState('');
+  const [password, setChangePass] = useState('');
+  const [isBtnDisabled, setisBtnDisabled] = useState(true);
+
+  useEffect(() => {
+    const validEmail = email.toLowerCase().match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+    const validPassword = password.length >= SIX;
+    if (validEmail && validPassword) {
+      setisBtnDisabled(false);
+    } else {
+      setisBtnDisabled(true);
+    }
+  }, [email, password]);
+
   return (
     <div>
       <Redirect to="/login" />
-      <h1>Login</h1>
+      <div>
+        <label htmlFor="email">
+          Email
+          <input
+            data-testid="common_login__input-email"
+            id="email"
+            type="email"
+            placeholder="Email"
+            onChange={ (e) => setChangeEmail(e.target.value) }
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="password">
+          Senha
+          <input
+            data-testid="common_login__input-password"
+            id="password"
+            type="password"
+            placeholder="Senha"
+            onChange={ (e) => setChangePass(e.target.value) }
+          />
+        </label>
+      </div>
+      <div>
+        <button
+          data-testid="common_login__button-login"
+          type="button"
+          disabled={ isBtnDisabled }
+        >
+          Login
+        </button>
+        <button
+          type="button"
+          data-testid="common_login__button-register"
+        >
+          Registrar
+        </button>
+      </div>
+      <p
+        data-testid="common_login__element-invalid-email"
+      >
+        Usuário inválido!
+      </p>
     </div>
   );
 }
