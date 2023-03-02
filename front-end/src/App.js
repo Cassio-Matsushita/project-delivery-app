da3b273e2df9e43042e3ba4c2491118988fc02a5
Cassio <superkoalabr@gmail.com>
2023-03-02 11:58:25 -0300

    inicio do back-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+
 import Products from './pages/Products';
 import './App.css';
-// import rockGlass from './images/rockGlass.svg';
+import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
     <div className="App">
-      {/* <span className="logo">TRYBE</span>
+      <span className="logo">TRYBE</span>
       <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
         Glass
-      </object> */}
+      </object>
 
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/login" component={ Login } />
+
         <Route exact path="/register" component={ Register } />
+
         <Route exact path="/customer/products" component={ Products } />
       </Switch>
 
--- front-end/src/api/fetchApi.js
+++ front-end/src/api/fetchApi.js
@@ -1,14 +1,11 @@
-export default async function login(email, password) {
-  try {
-    const { data } = await axios.post('http://localhost:3001/login', {
-      email,
-      password,
-    })
-      .catch((error) => {
-        console.error(error);
-      });
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-}
+import axios from 'axios';
+
+const login = async (email, password) => {
+  const { data } = await axios.post('https://localhost:3000/login', { email, password })
+    .catch((error) => {
+      console.error(error);
+    });
+  return data;
+};
+
+export default login;
